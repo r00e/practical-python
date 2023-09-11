127ca518f84e32450c884d335a986dc9ab43ac14
r00e <RuYig@outlook.com>
2023-09-11 21:56:11 +0000

    1.8 extra payments

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -12,3 +12,23 @@ while principal > 0:
   total_paid = total_paid + payment
 
 print('Total paid', total_paid)
+
+# Exercise 1.8
+principal_1_8 = 500000.0
+rate_1_8 = 0.05
+payment_1_8 = 2684.11
+total_paid_1_8 = 0.0
+first_year = 12
+months = 0
+
+while principal_1_8 > 0:
+  months = months + 1
+  principal_1_8 = principal_1_8 * ( 1 + rate / 12) - payment_1_8
+  if first_year > 0 :
+    principal_1_8 = principal_1_8 - 1000
+    total_paid_1_8 = total_paid_1_8 + 1000
+    first_year = first_year - 1
+  total_paid_1_8 = total_paid_1_8 + payment_1_8
+
+print('Total paid', total_paid_1_8)
+print('Total month', months)
