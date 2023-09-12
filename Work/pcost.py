fe87ce1b6fbcb8c6d4e01a3d2ea53aeccc643a08
r00e <RuYig@outlook.com>
2023-09-12 00:08:00 +0000

    1.27 pcost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+cost = 0.0
+
+with open('Data/portfolio.csv', 'rt') as f:
+  next(f)
+  for line in f:
+    info = line.split(',')
+    amount = int(info[1])
+    price = float(info[2])
+    cost = cost + amount * price
+
+print('Total cost', cost)
+
+
